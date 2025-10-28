28e190f2ee31186908eb8a1179fe9caa3a09c747
P Lê <phamle21@gmai.com>
2025-10-28 22:39:03 +0700

    feat: add db notifications

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -63,6 +63,7 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ]);
+            ])
+            ->databaseNotifications();
     }
 }
