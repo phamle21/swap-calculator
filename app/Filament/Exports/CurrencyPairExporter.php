68f0a59a4b7bb9e28285921b5beb06e9d43324b4
P Lê <phamle21@gmai.com>
2025-10-28 22:01:25 +0700

    feat: export currency pairs

---
+++ app/Filament/Exports/CurrencyPairExporter.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Filament\Exports;
+
+use App\Models\CurrencyPair;
+use Filament\Actions\Exports\ExportColumn;
+use Filament\Actions\Exports\Exporter;
+use Filament\Actions\Exports\Models\Export;
+use Illuminate\Support\Number;
+
+class CurrencyPairExporter extends Exporter
+{
+    protected static ?string $model = CurrencyPair::class;
+
+    public static function getColumns(): array
+    {
+        return [
+            ExportColumn::make('id')
+                ->label('ID'),
+            ExportColumn::make('symbol'),
+            ExportColumn::make('base'),
+            ExportColumn::make('quote'),
+            ExportColumn::make('is_active'),
+            ExportColumn::make('created_at'),
+        ];
+    }
+
+    public static function getCompletedNotificationBody(Export $export): string
+    {
+        $body = 'Your currency pair export has completed and ' . Number::format($export->successful_rows) . ' ' . str('row')->plural($export->successful_rows) . ' exported.';
+
+        if ($failedRowsCount = $export->getFailedRowsCount()) {
+            $body .= ' ' . Number::format($failedRowsCount) . ' ' . str('row')->plural($failedRowsCount) . ' failed to export.';
+        }
+
+        return $body;
+    }
+}
--- app/Filament/Resources/CurrencyPairs/Pages/ListCurrencyPairs.php
+++ app/Filament/Resources/CurrencyPairs/Pages/ListCurrencyPairs.php
@@ -2,9 +2,12 @@
 
 namespace App\Filament\Resources\CurrencyPairs\Pages;
 
+use App\Filament\Exports\CurrencyPairExporter;
 use App\Filament\Imports\CurrencyPairImporter;
 use App\Filament\Resources\CurrencyPairs\CurrencyPairResource;
 use Filament\Actions\CreateAction;
+use Filament\Actions\ExportAction;
+use Filament\Actions\Exports\Models\Export;
 use Filament\Actions\ImportAction;
 use Filament\Resources\Pages\ListRecords;
 
@@ -17,6 +20,8 @@ class ListCurrencyPairs extends ListRecords
         return [
             ImportAction::make()
                 ->importer(CurrencyPairImporter::class),
+            ExportAction::make()
+                ->exporter(CurrencyPairExporter::class),
             CreateAction::make(),
         ];
     }
